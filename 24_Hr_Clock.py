c5dcf72939d0c8a84405e840bd2904989b1abcba
Shain Sahagun <95956735+m3mentomor1@users.noreply.github.com>
2024-01-30 22:24:20 +0800

    Update 24_Hr_Clock.py

--- 24_Hr_Clock.py
+++ 24_Hr_Clock.py
@@ -3,7 +3,7 @@ import time
 print("\nCurrent Date & Time: ")
 
 while True:
-    # Displays the current date, time, & timezone:
+    # Displays the current date, time (24-hour format), & timezone:
     print(time.strftime('%m/%d/%Y, %a | %H:%M:%S %p (%Z %z)'),  
     end='\r')       # Keeps the printed output on the same line.
     
